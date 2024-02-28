27b68326a64eab180ed4af0b3fc6e6b5a7e24ad6
Viktor Sec <hey@viktorsec.com>
2024-02-28 21:28:43 +0100

    Improve semantics

--- generate.py
+++ generate.py
@@ -24,62 +24,58 @@ def time_ago(created_utc):
 output = '''
 <!DOCTYPE html>
 <html>
-<head>
-    <title>TREY</title>
-    <style>
-        #comics {
-            height: 100vh;
-            width: 100vw;
-            display: flex;
-            flex-direction: column;
-            gap: 1em;
-            overflow-y: scroll;
-            scroll-snap-type: y mandatory;
-            scroll-padding: 1em;
-        }
+    <head>
+        <title>TREY</title>
+        <style>
+            main {
+                height: 100vh;
+                width: 100vw;
+                display: flex;
+                flex-direction: column;
+                gap: 1em;
+                overflow-y: scroll;
+                scroll-snap-type: y mandatory;
+                scroll-padding: 1em;
+            }
 
-        .comic {
-            scroll-snap-align: start;
-            width: 100%;
-            min-height: 85vh;
-        }
+            .comic {
+                scroll-snap-align: start;
+                width: 100%;
+                min-height: 85vh;
+            }
 
-        .comic:first-child {
-            padding-top: 1em;
-        }
-        
-        .images {
-            display: flex;
-            flex-direction: row;
-            gap: 1em;
-            overflow-x: scroll;
-            /*scroll-snap-type: x proximity;
-            scroll-padding: 0 3vw;*/
-            padding: auto;
-        }
+            .comic:first-child {
+                padding-top: 1em;
+            }
+            
+            .images {
+                display: flex;
+                flex-direction: row;
+                gap: 1em;
+                overflow-x: scroll;
+                /*scroll-snap-type: x proximity;
+                scroll-padding: 0 3vw;*/
+                padding: auto;
+            }
 
-        .topbar {
-            min-height: 5vh;
-            font-size: 150%;    
-        }
+            .topbar {
+                min-height: 5vh;
+                font-size: 150%;    
+            }
 
-        .title {
-            font-size: 150%;
-        }
+            img {
+                scroll-snap-align: start;
+                min-width: 70vw;
+                max-height: 80vh;
+            }
 
-        img {
-            scroll-snap-align: start;
-            min-width: 70vw;
-            max-height: 80vh;
-        }
-
-        img:first-child {
-            padding-left: 10vw;
-        }
-    </style>
-</head>
-<body>
-    <div id="comics">
+            img:first-child {
+                padding-left: 10vw;
+            }
+        </style>
+    </head>
+    <body>
+        <main>
 '''
 
 for i, comic in enumerate(comics):
@@ -90,28 +86,30 @@ for i, comic in enumerate(comics):
         time_after = f''', {time_ago(comic['created_utc'])} after TREY'''
 
     output += f'''
-    <div class="comic">
-        <div class="topbar">
-            <div class="title">
-                <strong><a href="{comic['url']}">{title}</a></strong>
-                by {comic['author']}
-            </div>
-            #{i+1} / {len(comics)}{time_after}.<br>
-            {humanize.intcomma(comic['score'])} points, {len(comic['frames'])} panels.
-        </div>
-        <div class="images">
+            <div class="comic">
+                <div class="topbar">
+                    <h2>
+                        <a href="{comic['url']}">{title}</a>
+                        by {comic['author']}
+                    </h2>
+                    #{i+1} / {len(comics)}{time_after}.<br />
+                    {humanize.intcomma(comic['score'])} points, {len(comic['frames'])} panels.
+                </div>
+                <div class="images">
     '''
     for frame in comic['frames']:
-        output += f'<img src="{frame}">\n'
+        output += f'''
+                    <img src="{frame}" alt="a comic panel" />
+        '''
 
     output += '''
-        </div>
-    </div>
+                </div>
+            </div>
     '''
 
 output += '''
-</div>
-</body>
+        </main>
+    </body>
 </html>
 '''
 
